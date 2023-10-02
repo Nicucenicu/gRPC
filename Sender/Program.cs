c9fe99d05ecc77d60cce055a0098692094b3553a
Nicu <cabac2001@gmail.com>
2023-10-02 16:32:35 +0300

    Add project files.

---
+++ Sender/Program.cs
@@ -0,0 +1,30 @@
+﻿using Grpc.Net.Client;
+using System;
+using Part2;
+using System.Threading.Tasks;
+using GrpcServer;
+
+namespace Sender
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            var channel = GrpcChannel.ForAddress("https://localhost:5001");
+
+            var client = new GetData.GetDataClient(channel);
+            while (true)
+            {
+                Console.WriteLine("Sender");
+                Console.Write("Introduceti topicul: ");
+                string topic = Console.ReadLine();
+                Console.Write("Scrieti mesajul: ");
+                string message = Console.ReadLine();
+                var input = new ReceiveData { Topic = topic, Message = message };
+                var reply = await client.GetDataInfoAsync(input);
+                //Console.WriteLine($"{reply.Message}");
+            }
+            Console.ReadLine();
+        }
+    }
+}
